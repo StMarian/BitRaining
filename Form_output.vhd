6870904a3dada48b1c51bd884d61dbd93c341708
Andriy Yushchenko <1111andrey1111@gmail.com>
2017-04-21 20:55:56 +0300

    Update and rename Form_output_v2.vhd to Form_output.vhd

--- Form_output.vhd
+++ Form_output.vhd
@@ -1,9 +1,7 @@
 -------------------------------------------------------------------------------
 --
 -- Title       : Form_output
--- Design      : game_1
--- Author      : Kp0c
--- Company     : SYGMA. Inc
+-- Design      : BitRaining
 --
 -------------------------------------------------------------------------------
 --
@@ -34,16 +32,16 @@ architecture behavioral of form_output_v2 is
 	constant out_zero: 	std_logic_vector(6 downto 0) := "0111111";
 	constant out_one: 	std_logic_vector(6 downto 0) := "0000110";
 	constant out_two:	std_logic_vector(6 downto 0) := "1011011";
-	constant out_three: std_logic_vector(6 downto 0) := "1001111";
+	constant out_three:     std_logic_vector(6 downto 0) := "1001111";
 	constant out_four: 	std_logic_vector(6 downto 0) := "1100110";
 	constant out_five: 	std_logic_vector(6 downto 0) := "1101101";
 	constant out_six: 	std_logic_vector(6 downto 0) := "1111101";
-	constant out_seven: std_logic_vector(6 downto 0) := "0000111";
-	constant out_eight: std_logic_vector(6 downto 0) := "1111111";
+	constant out_seven:     std_logic_vector(6 downto 0) := "0000111";
+	constant out_eight:     std_logic_vector(6 downto 0) := "1111111";
 	constant out_nine: 	std_logic_vector(6 downto 0) := "1101111";
-	constant out_fail:  std_logic_vector(6 downto 0) := "1110001";	
-	constant IndicatorSignals :	outputSignal := (out_zero, out_one, out_two, out_three, out_four, out_five, out_six,
-	out_seven, out_eight, out_nine, out_fail);
+	constant out_fail:      std_logic_vector(6 downto 0) := "1110001";	
+	constant IndicatorSignals : outputSignal := (out_zero, out_one, out_two, out_three, out_four, out_five, out_six,
+		out_seven, out_eight, out_nine, out_fail);
 	
 begin 		  
 	process(CLK)
